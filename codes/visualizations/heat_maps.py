3ff4b16037460a98ff1bc19619af9c937a6e9453
Skanda-Aithal <skandakumar6688@gmail.com>
2023-03-07 21:04:58 +0530

    modified

--- codes/visualizations/heat_maps.py
+++ codes/visualizations/heat_maps.py
@@ -11,20 +11,17 @@ def hm_main(home_team, away_team,event_type):
     home_passes = add_locations(home_passes)
     away_passes = add_locations(away_passes)
     
-    #Unsuccessfull passes have value. But Success passes' values are 'nan'. So fill na values by 'Success'
     home_passes['pass_outcome'].fillna('Success', inplace=True)
     away_passes['pass_outcome'].fillna('Success', inplace=True)
     
-    #Create 4 pitches for heat and pass maps.
     pitch = Pitch_class()
     pitch, fig, axs = pitch.create_pitch(1,2)
     
-    #'kdeplot' method shows heat map.
-    pitch.kdeplot(home_passes['x'], home_passes['y'], ax=axs['pitch'][0][0],
+    pitch.kdeplot(home_passes['x'], home_passes['y'], ax=axs['pitch'][1][1],
                            shade=True, levels=100, shade_lowest=True,
                            cut=7, cmap='Reds')
     
-    pitch.kdeplot(away_passes['x'], away_passes['y'], ax=axs['pitch'][0][1],
+    pitch.kdeplot(away_passes['x'], away_passes['y'], ax=axs['pitch'][1][2],
                           shade=True, levels=100, shade_lowest=True,
                           cut=4, cmap='Blues')
      
